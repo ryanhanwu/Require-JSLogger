ee850753d2d79943d49e08a22473c0db2323fecb
Ryan Wu <flyworld@gmail.com>
2012-04-24 18:51:24 +0800

    init commit

---
+++ logger.js
@@ -0,0 +1,95 @@
+define(function() {
+  var Logger = {
+    DirObject : function() {
+      this._msg = "";
+      this.data = {};            
+      this.setMessage = function(msg) {
+        this._msg = msg;
+      }
+      this.setData = function(data) {
+        this.data = data;
+      }
+    },
+    defaultlogLevel: ['error', 'warn', 'log', 'info', 'dir', 'debug'],
+    infoLevel: ['log', 'info', 'dir'],
+    devLevel: {
+      'DEPLOY': 0,
+      'ERROR': 1,
+      'WARN': 2,
+      'INFO': 3,
+      'DEBUG': 4
+    },
+    currentLv: 4,
+    _currentLv: "",
+    setLevel: function(lv) {
+      this._currentLv = lv;
+      this.currentLv = this.devLevel[lv];
+      if (!(window.console != null)) {
+        window.console = {};
+        for (i in defaultlogLevel) {
+          if (typeof c[defaultlogLevel[i]] === 'function') {
+            window.console[defaultlogLevel[i]] = function() {
+              return true;
+            };
+          }
+        }
+      } else {
+        switch (this.currentLv) {
+        case 0:
+        case 1:
+        case 2:
+          this._updateConsole(this.currentLv);
+          break;
+        case 3:
+          this._updateConsole(this.currentLv + this.infoLevel.length);
+          break;
+        case 4:
+        default:
+          break;
+        }
+    }
+      (function(ori){
+          var dirobj = new this.DirObject();
+          
+          ori.dir_o = ori.dir;
+
+          ori.dir = function() {
+          var e = new Error();
+            if (arguments.length > 1 && e.stack != undefined) {
+              dirobj.setMessage(arguments[0]);
+              dirobj.setData(arguments[1]);
+              var caller_line = e.stack.split("\n")[2];
+              var index = caller_line.indexOf("at ");
+              var clean = caller_line.slice(index+2, caller_line.length);
+               console.debug(_.initial(clean.split(":"),1).join(":"));
+               ori.dir_o(dirobj);
+             } else {
+               ori.dir_o(arguments);
+             }
+           }
+      }).call(this, console);
+    },    
+    getLevel: function() {
+      return this.devLevel[this.currentLv];
+    },
+    testLogger: function() {
+      console.debug("Current Debug Level : " + this._currentLv);
+      console.error('errorTest');
+      console.warn('warnTest');
+      console.log('logTest');
+      console.info('infoTest');
+      console.dir('dirTest');
+      console.debug('debugTest');
+    },
+    _updateConsole: function(lv) {
+      for (var i = lv; i < this.defaultlogLevel.length; i++) {
+        if (typeof console[this.defaultlogLevel[i]] === 'function') {
+          console[this.defaultlogLevel[i]] = function() {
+            return true;
+          };
+        }
+      }
+    }
+  };
+  return Logger;
+});
